3affce91764c23492faa918b8a7544473af0643d
Anastasiia Vivtonenko <40969481@cuni.cz>
2023-09-24 21:50:47 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TicTacToe
+{
+    internal static class Program
+    {
+        /// Главная точка входа для приложения.
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            var startupForm = new Form2();
+            Application.Run(startupForm);
+
+            Application.Run(new Form1(startupForm.SelectedGridSize));
+        }
+    }
+}
